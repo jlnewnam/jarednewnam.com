eac65d04471e52c859acc766e484cc8f09b4b526
r3hab-media <jared.newnam@gmail.com>
2021-12-06 20:16:23 -0700

    added sharing ability to post

--- public/articles/js/article_js.js
+++ public/articles/js/article_js.js
@@ -5,3 +5,28 @@ fetch("../footer.html")
   .then(data => {
     document.querySelector("footer").innerHTML = data;
   });
+
+  const pageURL = window.location.href;
+  const pageTitle = document.title;
+
+  const shareData = {
+    title: 'jarednewnam.com',
+    text: pageTitle,
+    url: pageURL
+  }
+
+  //TODO - loop results of share click - make this a universal script call
+  const shareLinks = document.querySelector('.fa-share-alt');
+  // const resultPara = document.querySelector('.result');
+
+  // Share must be triggered by "user activation"
+  shareLinks.addEventListener('click', async () => {
+    navigator.share(shareData);
+    // try {
+    //   await navigator.share(shareData)
+    //   resultPara.textContent = 'MDN shared successfully'
+    // } catch(err) {
+    //   resultPara.textContent = 'Error: ' + err
+    // }
+  });
+
