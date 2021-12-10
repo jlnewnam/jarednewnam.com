a5889fe81044bac36d1516ba84b9b0e954a9051b
r3hab-media <jared.newnam@gmail.com>
2021-12-09 21:39:14 -0700

    blah

--- public/js/article-list.js
+++ public/js/article-list.js
@@ -3,47 +3,47 @@ const author = 'jared l newnam';
 const blogArticles = [{
     author: author,
     date: '11/24/2021',
-    title: 'a brief introduction to querySelectorAll',
+    title: 'the querySelectorAll DOM API',
     minutesToRead: 10,
     tags: ['javascript', 'html'],
     image: 'https://placekitten.com/1600/900',
     altText: 'image of kittens',
     teaser: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus blanditiis magni natus! Blanditiis quibusdam mod',
-    readMore: 'articles/brief-introduction-to-queryselectorall.html'
+    url: 'articles/brief-introduction-to-queryselectorall.html'
   },
   {
     author: author,
     date: '11/24/2021',
-    title: 'a brief introduction to querySelectorAll',
+    title: 'the difference between named and anonymous functions',
     minutesToRead: 10,
     tags: ['javascript', 'html'],
     image: 'https://placekitten.com/1601/900',
     altText: 'image of kittens',
     teaser: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus blanditiis magni natus! Blanditiis quibusdam mod',
-    readMore: 'articles/brief-introduction-to-queryselectorall.html'
+    url: 'articles/brief-introduction-to-queryselectorall.html'
   },
   {
     author: author,
     date: '11/24/2021',
-    title: 'a brief introduction to querySelectorAll',
+    title: 'css animations explained',
     minutesToRead: 10,
     tags: ['javascript', 'html'],
     image: 'https://placekitten.com/1602/900',
     altText: 'image of kittens',
     teaser: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus blanditiis magni natus! Blanditiis quibusdam mod',
-    readMore: 'articles/brief-introduction-to-queryselectorall.html'
+    url: 'articles/brief-introduction-to-queryselectorall.html'
   },
   {
     author: author,
     date: '11/24/2021',
-    title: 'a brief introduction to querySelectorAll',
+    title: 'where have the birds gone',
     minutesToRead: 10,
     tags: ['javascript', 'html'],
     image: 'https://placekitten.com/1603/900',
     altText: 'image of kittens',
     teaser: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus blanditiis magni natus! Blanditiis quibusdam mod',
-    readMore: 'articles/brief-introduction-to-queryselectorall.html'
-  } 
+    url: 'articles/brief-introduction-to-queryselectorall.html'
+  }
 ];
 
 
@@ -57,17 +57,18 @@ function blogArticlesTemplate(articles) {
           <span class="tease-post__meta--author">${articles.author}</span>
           <span class="tease-post__meta--date">${articles.date}</span>
         </h5>
-        <h2 class="tease-post__title"><a href="${articles.readMore}">${articles.title}</a></h2>
+        <h2 class="tease-post__title"><a href="${articles.url}">${articles.title}</a></h2>
         <div class="tease-post__details">
           <h4><i class="fa fa-clock"></i> ${articles.minutesToRead} minute read</h4>
           <h4><i class="fa fa-tag"></i> ${articles.tags}</h4>
+          <h4><i class="fa fa-share-alt"></i> Share</h4>
         </div>
         <hr>
         <img class="tease-post__img" src="${articles.image}" alt="${articles.altText}">
         <div class="tease-post__text">
           <p>${articles.teaser}</p>
 
-          <a href="${articles.readMore}" class="tease-post__more">[read more]</a>
+          <a href="//${articles.url}" class="tease-post__more">[read more]</a>
         </div>      
       </div>
     </div>    
@@ -77,3 +78,32 @@ function blogArticlesTemplate(articles) {
 document.getElementById('blogTeaserList').innerHTML = `
   ${blogArticles.map(blogArticlesTemplate).join('')}
   `;
+
+//sharing
+const pageURL = window.location.href;
+const pageTitle = document.title;
+
+const shareData = {
+  title: "jarednewnam.com",
+  text: blogArticles.map(titles => titles.title),
+  url: blogArticles.map(urls => urls.readMore)
+};
+
+const shareButtons = document.querySelectorAll(".fa-share-alt");
+
+if ("share" in navigator) {
+  shareButtons.forEach((shareButton) => {
+    shareButton.addEventListener("click", () => {
+      navigator
+        .share(shareData)
+        .then(() => {
+          console.log("Shared", shareData);
+        })
+        .catch(console.error);
+    });
+  });
+} else {
+  shareButtons.forEach((shareButton) => {
+    shareButton.style.display = "none";
+  });
+}
